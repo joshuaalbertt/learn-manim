401ad16c0d90a48abaa1c9d10243670cc5086b59
Joshua Albert <joshuaalbertdjayasurbakti@gmail.com>
2026-04-03 13:44:30 +0700

    Add partial movie file list for Updaters2 in FFMPEG format

--- learn1.py
+++ learn1.py
@@ -54,3 +54,35 @@ class Updaters(Scene):
 		self.wait(1)
 		mathtext.clear_updaters()
 		self.play(rectangle.animate.shift(RIGHT*2+UP*3), run_time = 2)
+
+#4. Updaters2
+class Updaters2(Scene):
+	def construct(self):
+    # Tracks the value of the radius
+		r = ValueTracker(0.5)
+
+		circle = always_redraw(lambda:
+			Circle(radius = r.get_value(), stroke_color = YELLOW, stroke_width = 5)
+		)
+
+		line_radius = always_redraw(lambda:
+			Line(start = circle.get_center(), end = circle.get_bottom(), stroke_color = RED_B, stroke_width = 10)
+		)
+
+		line_circumference = always_redraw(lambda:
+			Line(stroke_color = WHITE, stroke_width = 5
+			).set_length(2 * r.get_value() * PI).next_to(circle, DOWN, buff=0.2)
+		)
+
+		triangle = always_redraw(lambda:
+			Polygon(circle.get_bottom(), circle.get_left(), circle.get_right(), fill_color = GREEN_C)
+		)
+
+		self.play(LaggedStart(
+			Create(circle), DrawBorderThenFill(line_radius), DrawBorderThenFill(triangle),
+			run_time = 4, lag_ratio = 0.75
+		))
+
+		self.play(ReplacementTransform(circle.copy(), line_circumference), run_time = 2)
+		self.play(r.animate.set_value(3), run_time = 3)
+
